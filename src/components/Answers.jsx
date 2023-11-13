5977fdd32fd720cd8e323187c72ec569731ce212
Brian Smith <briansmith@rocketcentral.com>
2023-11-13 10:35:43 -0500

    Working on Answers Component

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -15,33 +15,35 @@ export default function Answers({
 
   return (
     <ul id="answers">
-      {shuffledAnswers.current.map((answer) => {
-        const isSelected = selectedAnswer === answer;
-        let cssClass = '';
+      {shuffledAnswers.current.map(
+        (answer) => {
+          const isSelected = selectedAnswer === answer;
+          let cssClass = '';
 
-        if (answerState === 'answered' && isSelected) {
-          cssClass = 'selected';
-        }
+          if (answerState === 'answered' && isSelected) {
+            cssClass = 'selected';
+          }
+  
+          if (
+            (answerState === 'correct' || answerState === 'wrong') &&
+            isSelected
+          ) {
+            cssClass = answerState;
+          }
 
-        if (
-          (answerState === 'correct' || answerState === 'wrong') &&
-          isSelected
-        ) {
-          cssClass = answerState;
-        }
-
-        return (
-          <li key={answer} className="answer">
-            <button
-              onClick={() => onSelect(answer)}
-              className={cssClass}
-              disabled={answerState !== ''}
-            >
-              {answer}
-            </button>
-          </li>
-        );
-      })}
+          return (
+            <li key={answer} className="answer">
+              <button
+                onClick={() => onSelect(answer)}
+                className={cssClass}
+                disabled={answerState !== ''}
+              >
+                {answer}
+              </button>
+            </li>
+          );
+        })
+      }
     </ul>
   );
 }
